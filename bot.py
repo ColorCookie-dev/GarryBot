5469b31c97450b6a8bcbda793726dac5efe7c96f
Color Cookie <colorcookie@protonmail.com>
2020-05-11 20:31:09 +0530

    Removed quick commands, added webhook support in gs command

--- bot.py
+++ bot.py
@@ -7,69 +7,67 @@ from google_images_search import GoogleImagesSearch
 
 bot = commands.Bot(command_prefix='~')
 
-quick_react_dict = {
-    'notaccident': ('https://i.kym-cdn.com/entries/icons/facebook/000/023/853/accidents.jpg', 'There are no accidents'),
-    'bigbrain': ('https://vignette.wikia.nocookie.net/furthestextreme/images/f/f6/Cropped-big-brain.jpg/revision/latest/scale-to-width-down/340?cb=20190929085659', 'Big brains'),
-    'bigbrain2': ('https://i.kym-cdn.com/entries/icons/original/000/030/525/cover5.png', 'Markiplier version of big brain'),
-    'coffindance': ('https://media0.giphy.com/media/j6ZlX8ghxNFRknObVk/giphy.gif?cid=ecf05e471a1b565aa6951015ad6c9ec2eb2de8a9bed0465d&rid=giphy.gif', 'coffin dance'),
-}
-
-# Just for the intellisense
-class Quick_Reactions(commands.Cog):
-    pass
-
-dyn_quick_functions_code = "\
-class Quick_Reactions(commands.Cog, name='Quick'):\n\
-    '''For Quick Reactions to memes'''\n"
-
-for i in quick_react_dict:
-    url, meme = quick_react_dict[i]
-
-    dyn_quick_functions_code += f"\
-    @commands.command()\n\
-    async def {i}(self, ctx):\n\
-        '''embeds {meme} meme'''\n\
-        embed = discord.Embed()\n\
-        embed.set_image(url='{url}')\n\
-        await ctx.send(embed=embed)\n"
-
-exec(dyn_quick_functions_code)
-
 class Searching_Commands(commands.Cog, name="Search"):
     '''Commands which search on the web'''
 
     @commands.command()
-    async def gs(self, ctx, ind: typing.Optional[int] = 1, *, searchTerm):
+    async def gs(self,
+            ctx,
+            start: typing.Optional[int] = 1,
+            num: typing.Optional[int] = 1,
+            *,
+            searchTerm,
+            delete=False):
         '''Searches the phrase given on google'''
 
-        if not (ind >= 1 and ind <= 200):
-            await ctx.send('Index not in bound')
+        if not (start >= 1 and start <= 200 and num >= 1 and num <= 10):
+            await ctx.send('Numbers not in bound')
             return
 
         gis = GoogleImagesSearch(secret_api_key, secret_cx_code)
 
         _search_params = {
             'q': searchTerm,
-            'start': ind,
-            'num': 1,
+            'start': start,
+            'num': num,
             'safe': 'high',
         }
 
-        gis.search(search_params=_search_params)
-        results = gis.results()
-        url = results[0].url if len(results) >= 1 else None
-        if url:
-            embed = discord.Embed()
-            embed.set_image(url=url)
-            await ctx.send(embed=embed)
+        webhook = None
+        webhooks = await ctx.channel.webhooks()
+        if len(webhooks):
+            webhook = webhooks[0]
         else:
-            await ctx.send('Couldn\'t find the searched image.')
+            webhook = await ctx.channel.create_webhook(name='LOLBot')
+
+        gis.search(search_params=_search_params)
+        embeds = []
+        for img in gis.results():
+            if img.url:
+                embed_data = {
+                    'type': 'image',
+                    'image': {
+                        'url': img.url,
+                    },
+                }
+                embeds.append(discord.Embed.from_dict(embed_data))
+        await ctx.message.delete()
+        if not delete:
+            cont = ctx.message.content
+            await webhook.send(content=cont,
+                    embeds=embeds,
+                    username=ctx.message.author.nick,
+                    avatar_url=ctx.message.author.avatar_url)
+        else:
+            await webhook.send(
+                    embeds=embeds,
+                    username=ctx.message.author.nick,
+                    avatar_url=ctx.message.author.avatar_url)
 
     @commands.command()
-    async def gsd(self, ctx, ind: typing.Optional[int] = 1, *, arg):
+    async def gsd(self, ctx, start: typing.Optional[int] = 1, num: typing.Optional[int] = 1, *, arg):
         '''Same as gs but deletes the original message'''
-        await self.gs(ctx, ind=ind, searchTerm=arg)
-        await ctx.message.delete()
+        await self.gs(ctx, start=start, num=num, searchTerm=arg, delete=True)
 
 class Bot_Commands(commands.Cog, name="Normal"):
     '''Normal Bot commands'''
@@ -90,7 +88,6 @@ class Bot_Commands(commands.Cog, name="Normal"):
         '''shows the version'''
         await ctx.send('Better than your mom!')
 
-bot.add_cog(Quick_Reactions(bot))
 bot.add_cog(Searching_Commands(bot))
 bot.add_cog(Bot_Commands(bot))
 
