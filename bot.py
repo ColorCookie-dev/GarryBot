42e328318e25917fcdf2272a070b04c507e58b74
Color Cookie <colorcookie@protonmail.com>
2020-05-14 18:47:48 +0530

    Database separation

--- bot.py
+++ bot.py
@@ -1,31 +1,9 @@
-import sqlite3
 import discord
 import typing
 from discord.ext import commands
 from discord_token import secret_token, secret_api_key, secret_cx_code
+from database import DBConnection
 from google_images_search import GoogleImagesSearch
-
-class DBConnection:
-    insert_query = 'INSERT INTO links (term, ind, href) VALUES ("%s", "%d", "%s")'
-    select_query = 'SELECT href FROM links WHERE term="%s" and ind="%d"'
-    update_query = 'UPDATE links SET href="%s" where term="%s"'
-
-    def __init__(self, db=':memory:'):
-        self.conn = sqlite3.connect(db)
-        c = self.conn.cursor()
-        # Create table command
-        try:
-            c.execute('''CREATE TABLE links
-                         (id int primary key autoincrement, term text not null, ind int, href text)''')
-            self.conn.commit()
-        except sqlite3.OperationalError:
-            pass
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, type, value, tb):
-        self.conn.close()
 
 bot = commands.Bot(command_prefix='~')
 
